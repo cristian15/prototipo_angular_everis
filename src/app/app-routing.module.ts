df2cba5a154c776a6dc04192a13f952863b2d297
Cristian Beltran <cbeltranc6@gmail.com>
2020-09-22 03:05:21 -0300

    Prototipo Angular Beca JAVA2002

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { ReporteComponent } from './components/reporte/reporte.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'principal', component:PrincipalComponent ,
+      children:[
+        {path:'reportes', component:ReportesComponent, 
+          children:[
+            {path:'', component:ReporteComponent},
+            {path:'categoria', component:CategoriaComponent}
+            ]},
+        {path:'bienvenida', component:BienvenidaComponent},
+        {path:'', component:BienvenidaComponent},
+      ]
+  },
+  { path:'', component:InicioComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+import { ReporteComponent } from './components/reporte/reporte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    PrincipalComponent,
+    BienvenidaComponent,
+    InicioComponent,
+    ReportesComponent,
+    CategoriaComponent,
+    ReporteComponent
   ],
   imports: [
     BrowserModule,
